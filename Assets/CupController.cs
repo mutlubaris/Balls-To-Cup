65be94b54fd35921037b261c5987f32fe0cfe54d
Baris Mutlu <mutlubaris_1@hotmail.com>
2023-03-27 10:32:25 +0300

    cup rotation

--- Assets/CupController.cs
+++ Assets/CupController.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml;
@@ -5,32 +6,31 @@ using UnityEngine;
 
 public class CupController : MonoBehaviour
 {
-    [SerializeField] private float _sensitivity;
+    [SerializeField] private float _sensitivity = 1;
     [SerializeField] private MeshFilter _bodyMeshFilter;
     [SerializeField] private Transform _bodyTrans;
 
-    private Vector3 _clickPosition;
+    private Vector3 _previousMousePos;
 
-    private void Start()
-    {
-        var oldPos = transform.position;
-        transform.position = new Vector3(transform.position.x, _bodyMeshFilter.mesh.bounds.center.y, transform.position.z);
-        _bodyTrans.localPosition += Vector3.up * (oldPos.y - _bodyMeshFilter.mesh.bounds.center.y);
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            _clickPosition = Input.mousePosition;
-            print("Obj Pos: " + Camera.main.WorldToScreenPoint(transform.position));
-            print("Click Pos: " + _clickPosition);
+            _previousMousePos = Input.mousePosition;
         }
 
         if (Input.GetMouseButton(0))
         {
-            
+            Vector3 vectorToPoint = _previousMousePos - Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 normalVector = Input.mousePosition - _previousMousePos;
+            transform.Rotate(new Vector3(0, 0, Vector3.Cross(vectorToPoint, normalVector).z) * Time.deltaTime * _sensitivity);
+            _previousMousePos = Input.mousePosition;
         }
+    }
+
+    [Button]
+    private void SetAnchor()
+    {
+        _bodyTrans.localPosition = Vector3.up * (-_bodyMeshFilter.mesh.bounds.center.y);
     }
 }
