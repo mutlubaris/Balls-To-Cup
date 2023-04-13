8b9550ded4559f72865d5468f0941770e597653d
Baris Mutlu <mutlubaris_1@hotmail.com>
2023-04-13 16:11:05 +0300

    sens panel

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -2,26 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour
 {
     [SerializeField] private SensitivityController _sensitivityController;
     [SerializeField] private TextMeshProUGUI _sensitivityValueText;
+    [SerializeField] private int _minSensitivity = 1;
+    [SerializeField] private int _maxSensitivity = 11;
+    [SerializeField] private Button decrementButton;
+    [SerializeField] private Button incrementButton;
 
     private void Start()
     {
-        _sensitivityValueText.SetText(_sensitivityController.rotationSensitivity.ToString());
+        UpdatePanel();
     }
 
     public void IncreaseSensitivity()
     {
         _sensitivityController.rotationSensitivity++;
-        _sensitivityValueText.SetText(_sensitivityController.rotationSensitivity.ToString());
+        UpdatePanel();
     }
 
     public void DecreaseSensitivity() 
     {
         _sensitivityController.rotationSensitivity--;
+        UpdatePanel();
+    }
+
+    private void UpdatePanel()
+    {
         _sensitivityValueText.SetText(_sensitivityController.rotationSensitivity.ToString());
+        if (_sensitivityController.rotationSensitivity <= _minSensitivity) decrementButton.interactable = false;
+        else decrementButton.interactable = true;
+        if (_sensitivityController.rotationSensitivity >= _maxSensitivity) incrementButton.interactable = false;
+        else incrementButton.interactable = true;
     }
 }
